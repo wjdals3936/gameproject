cbde0d905c4e4ffb61e15139a1d87c8ce5b09d2a
junhyeok83 <101380588+junhyeok83@users.noreply.github.com>
2022-05-17 22:30:59 +0900

    Add files via upload

--- 사진파일/임시시작화면.py
+++ 사진파일/임시시작화면.py
@@ -1,3 +1,4 @@
+from turtle import width
 import pygame
 import time
 import sys
@@ -11,9 +12,14 @@ startImg = pygame.image.load("starticon.png")
 quitImg = pygame.image.load("quiticon.png")
 clickStartImg = pygame.image.load("clickedStartIcon.png")
 clickQuitImg = pygame.image.load("clickedQuitIcon.png")
+vanillaImg = pygame.image.load("vanilla.png")
+flip_vanillaImg = pygame.transform.flip(vanillaImg, True, False)
+shotingstarImg = pygame.image.load("shotingstar.png")
+flip_shotingstar = pygame.transform.flip(shotingstarImg, True, False)
 
-display_width = 800
-display_height = 600
+
+display_width = 640
+display_height = 480
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption("제목")
 
@@ -33,6 +39,14 @@ class Button:
             gameDisplay.blit(img_in, (x, y))
 
 
+# class Background(self, img_in, x, y, width, pygame.freetype.height, img_act):
+    #background1 = gameDisplay.blit(vanillaImg, (20, 10))
+    #background2 = gameDisplay.blit(shotingstarImg, (20, 370))
+    # pygame.time.delay()(1000)
+    #background1 = gameDisplay.blit(flip_vanillaImg, (20, 10))
+    #background2 = gameDisplay.blit(flip_shotingstar, (20, 370))
+
+
 def quitgame():
     pygame.quit()
     sys.exit()
@@ -50,13 +64,18 @@ def mainmenu():
 
         gameDisplay.fill(white)
 
-        titletext = gameDisplay.blit(titleImg, (220, 150))
-        startButton = Button(startImg, 280, 260, 60, 20,
-                             clickStartImg, 273, 258, None)
-        quitButton = Button(quitImg, 445, 260, 60, 20,
-                            clickQuitImg, 440, 258, quitgame)
+        titletext = gameDisplay.blit(titleImg, (20, 190))
+        background1 = gameDisplay.blit(vanillaImg, (20, 10))
+        background2 = gameDisplay.blit(shotingstarImg, (20, 370))
+        #backgroun1 = Background(vanillaImg, )
+        startButton = Button(startImg, 510, 104, 60, 20,
+                             clickStartImg, 510, 108, None)
+        quitButton = Button(quitImg, 510, 370, 60, 20,
+                            clickQuitImg, 510, 374, quitgame)
         pygame.display.update()
         clock.tick(15)
+        pos = pygame.mouse.get_pos()
+        print(pos)
 
 
 mainmenu()
